46f329c0a91d7f024004c709ad7c6c10dfc05c3b
Ricardo Porrata <poricardo1031@gmail.com>
2019-12-22 14:02:27 -0400

    Update dosage controller and model

--- controllers/dosageController.js
+++ controllers/dosageController.js
@@ -5,7 +5,9 @@ module.exports = {
   getDosagesOptions: function(req, res) {
     try {
       
-      let sql = `SELECT * FROM Dosage`;
+      let sql = `SELECT DISTINCT Dosage as Amount
+                 FROM MedicineDosage 
+                 ORDER BY Dosage ASC;`;
       let dosageOptions = new Array(DosageModel);
 
       db.connection.query(sql, (err, results) => {
@@ -13,15 +15,17 @@ module.exports = {
           throw err;
         } else {
 
+          console.log({list: results});
           results.forEach((element, index, arr) => {
             dosageOptions[index] = (element);
+        
           });
 
           res.json({dosageOptions});
         }
       });
     } catch (error) {
-      res.json({ message: erorr });
+      res.json({ message: error });
     }
   }
 };
--- models/Dosage.js
+++ models/Dosage.js
@@ -4,7 +4,6 @@ const db = require('../db');
 
 class Dosage extends Model {}
 Dosage.init({
-  Dosage_ID: {type:DataTypes.INTEGER, primaryKey: true},
   Amount: DataTypes.INTEGER,
 
 }, { sequelize: db.sequelize, modelName: 'Dosage' });
