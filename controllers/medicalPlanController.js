82fbc9e0aec4a28f62cc367f64bd1b39701276d6
Ricardo Porrata <poricardo1031@gmail.com>
2019-12-26 13:15:28 -0400

    Edited query in medical plan getMedicalPlanOptions function

--- controllers/medicalPlanController.js
+++ controllers/medicalPlanController.js
@@ -4,13 +4,13 @@ const MedicalPlanModel = require("../models/MedicalPlan");
 module.exports = {
   getMedicalPlanOptions: function(req, res) {
     try {
-      let sql = `SELECT DISTINCT Name as MedicalPlan
+      let sql = `SELECT DISTINCT *
                  FROM MedicalPlans 
                  ORDER BY Name ASC`;
 
       db.connection.query(sql, (error, results) => {
         if (error) {
-          throw erorr;
+          throw error;
         } else {
           res.json(results);
         }
